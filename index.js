ef0d4aef55023bd73f67b696ff2df74e40edf83b
yuyutti <yuyutti@yuyutti.com>
2023-09-07 22:28:09 +0900

    commits

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const SteinStore = require("stein-js-client");
-const store = new SteinStore("https://api.steinhq.com/v1/storages/63eb636aeced9b09e9beb441");
+const ECS = new SteinStore("https://api.steinhq.com/v1/storages/64f9cb3beced9b09e9ef9e3f");
+const ECG_competitive = new SteinStore("https://api.steinhq.com/v1/storages/64f9cb6ed27cdd09f0146222");
+const ECS_nurturing = new SteinStore("https://api.steinhq.com/v1/storages/64f9cb55eced9b09e9ef9e4e");
 
 process.on('uncaughtException', function(err) {
   console.log(err);
@@ -9,26 +11,78 @@ process.on('uncaughtException', function(err) {
 
 app.use(express.static('./'));
 
-let send_data
+let send_data_ecs
+let send_data_ecg_c
+let send_data_ecg_n
 
 app.get('/',(req, res) => {
-  store.read("index", {authentication: { username: "yuyuttikunnhatennsainanodaxa", password: "fwuasicjnks@fecknfaweioknmjsedfvnihowfveomfowe" }}).then(data => {
-    send_data = data
-  });
-  res.sendFile(__dirname + "/html/index.html")
+  let id = req.query.id;
+  if (id !== 'ecs' && id !== 'ecg_c' && id !== 'ecg_n') id = 'ecs';
+  if(id === "ecs"){
+    ECS.read("index", {authentication: { username: "yuyutti", password: "yuyutti0625" }}).then(data => {
+      send_data_ecs = data
+    });
+    res.sendFile(__dirname + "/html/index.html")
+  }
+  if(id === "ecg_c"){
+    ECG_competitive.read("index", {authentication: { username: "yuyutti", password: "yuyutti0625" }}).then(data => {
+      send_data_ecg_c = data
+    });
+    res.sendFile(__dirname + "/html/index.html")
+  }
+  if(id === "ecg_n"){
+    ECS_nurturing.read("index", {authentication: { username: "yuyutti", password: "yuyutti0625" }}).then(data => {
+      send_data_ecg_n = data
+    });
+    res.sendFile(__dirname + "/html/index.html")
+  }
 })
 
 app.get('/data',(req,res) => {
-  if (send_data && Array.isArray(send_data)) {
-    const hasPlayerName = send_data.every(item => item.playername);
-    if (hasPlayerName) {
-      res.json(send_data);
-      send_data = null;
+  let id = req.query.id;
+  if(id === "ecs"){
+    if (send_data_ecs && Array.isArray(send_data_ecs)) {
+      const hasPlayerName = send_data_ecs.every(item => item.playername);
+      if (hasPlayerName) {
+        res.json(send_data_ecs);
+        send_data_ecs = null;
+      } else {
+        res.status(400).json({ error: 'playername property is missing in some elements' });
+      }
     } else {
-      res.status(400).json({ error: 'playername property is missing in some elements' });
+      res.status(400).json({ error: 'send_data is not defined or is not an array' });
     }
-  } else {
-    res.status(400).json({ error: 'send_data is not defined or is not an array' });
+    return;
+  }
+
+  if(id === "ecg_c"){
+    if (send_data_ecg_c && Array.isArray(send_data_ecg_c)) {
+      const hasPlayerName = send_data.every(item => item.playername);
+      if (hasPlayerName) {
+        res.json(send_data_ecg_c);
+        send_data_ecg_c = null;
+      } else {
+        res.status(400).json({ error: 'playername property is missing in some elements' });
+      }
+    } else {
+      res.status(400).json({ error: 'send_data is not defined or is not an array' });
+    }
+    return;
+  }
+
+  if(id === "ecg_n"){
+    if (send_data_ecg_n && Array.isArray(send_data_ecg_n)) {
+      const hasPlayerName = send_data_ecg_n.every(item => item.playername);
+      if (hasPlayerName) {
+        res.json(send_data_ecg_n);
+        send_data_ecg_n = null;
+      } else {
+        res.status(400).json({ error: 'playername property is missing in some elements' });
+      }
+    } else {
+      res.status(400).json({ error: 'send_data is not defined or is not an array' });
+    }
+    return;
   }
 })
 
