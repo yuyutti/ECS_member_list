553990488ab65a50d97149f15e18c226165eb304
yuyutti <yuyutti@yuyutti.com>
2023-09-07 23:04:12 +0900

    commits

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const SteinStore = require("stein-js-client");
 const ECS = new SteinStore("https://api.steinhq.com/v1/storages/64f9cb3beced9b09e9ef9e3f");
 const ECG_competitive = new SteinStore("https://api.steinhq.com/v1/storages/64f9cb6ed27cdd09f0146222");
-const ECS_nurturing = new SteinStore("https://api.steinhq.com/v1/storages/64f9cb55eced9b09e9ef9e4e");
+const ECG_nurturing = new SteinStore("https://api.steinhq.com/v1/storages/64f9cb55eced9b09e9ef9e4e");
+const ECG_zb = new SteinStore("https://api.steinhq.com/v1/storages/64f9d664eced9b09e9efa08d");
 
 process.on('uncaughtException', function(err) {
   console.log(err);
@@ -14,32 +15,49 @@ app.use(express.static('./'));
 let send_data_ecs
 let send_data_ecg_c
 let send_data_ecg_n
+let send_data_ecg_zb
 
 app.get('/',(req, res) => {
   let id = req.query.id;
-  if (id !== 'ecs' && id !== 'ecg_c' && id !== 'ecg_n') id = 'ecs';
+  if (id !== 'ecs' && id !== 'ecg_c' && id !== 'ecg_n' && id !== 'ecg_zb') id = 'ecs';
+
   if(id === "ecs"){
     ECS.read("index", {authentication: { username: "yuyutti", password: "yuyutti0625" }}).then(data => {
       send_data_ecs = data
     });
     res.sendFile(__dirname + "/html/index.html")
   }
+
   if(id === "ecg_c"){
     ECG_competitive.read("index", {authentication: { username: "yuyutti", password: "yuyutti0625" }}).then(data => {
       send_data_ecg_c = data
     });
     res.sendFile(__dirname + "/html/index.html")
   }
+
   if(id === "ecg_n"){
-    ECS_nurturing.read("index", {authentication: { username: "yuyutti", password: "yuyutti0625" }}).then(data => {
+    ECG_nurturing.read("index", {authentication: { username: "yuyutti", password: "yuyutti0625" }}).then(data => {
       send_data_ecg_n = data
     });
     res.sendFile(__dirname + "/html/index.html")
   }
+
+  if(id === "ecg_zb"){
+    ECG_zb.read("index", {authentication: { username: "yuyutti", password: "yuyutti0625" }}).then(data => {
+      send_data_ecg_zb = data
+    });
+    res.sendFile(__dirname + "/html/index.html")
+  }
+
 })
 
 app.get('/data',(req,res) => {
   let id = req.query.id;
+
+  if(id !== "ecs" && id !== "ecg_c" && id !== "ecg_n" && id !== "ecg_zb"){
+    res.status(404).json({ error: 'page not fund' });
+  }
+
   if(id === "ecs"){
     if (send_data_ecs && Array.isArray(send_data_ecs)) {
       const hasPlayerName = send_data_ecs.every(item => item.playername);
@@ -57,7 +75,7 @@ app.get('/data',(req,res) => {
 
   if(id === "ecg_c"){
     if (send_data_ecg_c && Array.isArray(send_data_ecg_c)) {
-      const hasPlayerName = send_data.every(item => item.playername);
+      const hasPlayerName = send_data_ecg_c.every(item => item.playername);
       if (hasPlayerName) {
         res.json(send_data_ecg_c);
         send_data_ecg_c = null;
@@ -76,6 +94,21 @@ app.get('/data',(req,res) => {
       if (hasPlayerName) {
         res.json(send_data_ecg_n);
         send_data_ecg_n = null;
+      } else {
+        res.status(400).json({ error: 'playername property is missing in some elements' });
+      }
+    } else {
+      res.status(400).json({ error: 'send_data is not defined or is not an array' });
+    }
+    return;
+  }
+
+  if(id === "ecg_zb"){
+    if (send_data_ecg_zb && Array.isArray(send_data_ecg_zb)) {
+      const hasPlayerName = send_data_ecg_zb.every(item => item.playername);
+      if (hasPlayerName) {
+        res.json(send_data_ecg_zb);
+        send_data_ecg_zb = null;
       } else {
         res.status(400).json({ error: 'playername property is missing in some elements' });
       }
